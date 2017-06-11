91abc814e0862ac63c0cba8e92935d8d44dd3b4b
Carlin Scuderi <carlinscuderi@gmail.com>
2017-06-11 11:05:22 -0400

    Tweaked some comments

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -3,18 +3,17 @@
 echo "do not run this script in one go. hit ctrl-c NOW"
 read -n 1
 
-##############################################################################################################
-### XCode Command Line Tools
-#      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
-
+# /////////////////////////////////////////////////////////////////////
+# XCode CLI tools
+# /////////////////////////////////////////////////////////////////////
 if ! xcode-select --print-path &> /dev/null; then
 
-		# Prompt user to install the XCode Command Line Tools
+		# Prompt to install the XCode CLI tools
 		xcode-select --install &> /dev/null
 
 		# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-		# Wait until the XCode Command Line Tools are installed
+		# Wait until the XCode CLI tools are installed
 		until xcode-select --print-path &> /dev/null; do
 			sleep 5
 		done
@@ -39,99 +38,88 @@ if ! xcode-select --print-path &> /dev/null; then
 		print_result $? 'Agree with the XCode Command Line Tools licence'
 
 	fi
-###
-##############################################################################################################
+# /////////////////////////////////////////////////////////////////////
 
 
 
-##############################################################################################################
-### homebrew!
+# /////////////////////////////////////////////////////////////////////
+# Homebrew
+# /////////////////////////////////////////////////////////////////////
 
-# (if your machine has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
+# If your machine has /usr/local locked down (like Google's), you can do this to place everything in ~/.homebrew
 mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
 export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
 
-# install all the things
+# Install all the things
 ./brew.sh
 ./brew-cask.sh
 
-### end of homebrew
-##############################################################################################################
+# /////////////////////////////////////////////////////////////////////
 
 
 
-
-##############################################################################################################
-### install of common things
-###
+# /////////////////////////////////////////////////////////////////////
+# Install other common things
+# /////////////////////////////////////////////////////////////////////
 
 # github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
 # bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
-
 # Type `git open` to open the GitHub page or website for a repository.
 npm install -g git-open
 
-# trash as the safe `rm` alternative
+# Trash as the safe `rm` alternative
 npm install --global trash-cli
 
-
-# github.com/rupa/z   - oh how i love you
+# Z
+# github.com/rupa/z
 git clone https://github.com/rupa/z.git ~/code/z
 chmod +x ~/code/z/z.sh
-# consider reusing your current .z file if possible. it's painful to rebuild :)
+# Consider reusing your current .z file if possible
 # z is hooked up in .bash_profile
 
-
-# for the c alias (syntax highlighted cat)
+# For the c alias (syntax highlighted cat)
 sudo easy_install Pygments
 
-
-# change to bash 4 (installed by homebrew)
+# Change to bash 4 (installed by homebrew)
 BASHPATH=$(brew --prefix)/bin/bash
 sudo echo $BASHPATH >> /etc/shells
-chsh -s $BASHPATH # will set for current user only.
-echo $BASH_VERSION # should be 4.x not the old 3.2.X
+chsh -s $BASHPATH
+echo $BASH_VERSION
 # Later, confirm iterm settings aren't conflicting.
 
-# setting up the sublime symlink
+# Setting up the sublime symlink
 ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
-###
-##############################################################################################################
+# /////////////////////////////////////////////////////////////////////
 
 
 
-##############################################################################################################
-### remaining configuration
-###
+# /////////////////////////////////////////////////////////////////////
+# Remaining config
+# /////////////////////////////////////////////////////////////////////
 
-# go read mathias, paulmillr, gf3, alraa's dotfiles to see what's worth stealing.
-
+# TODO: Maybe read mathias, paulmillr, gf3, alraa's dotfiles to see what's worth stealing
 # prezto and antigen communties also have great stuff
-#   github.com/sorin-ionescu/prezto/blob/master/modules/utility/init.zsh
+# github.com/sorin-ionescu/prezto/blob/master/modules/utility/init.zsh
 
-# set up osx defaults
-#   maybe something else in here https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
+# Set up osx defaults
+# Might be more: https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
 sh .osx
 
-# setup and run Rescuetime!
-
-###
-##############################################################################################################
+# /////////////////////////////////////////////////////////////////////
 
 
 
-##############################################################################################################
-### symlinks to link dotfiles into ~/
-###
+# /////////////////////////////////////////////////////////////////////
+# symlinks to link dotfiles into ~/
+# /////////////////////////////////////////////////////////////////////
 
-#   move git credentials into ~/.gitconfig.local    	http://stackoverflow.com/a/13615531/89484
-#   now .gitconfig can be shared across all machines and only the .local changes
+# Move git credentials into ~/.gitconfig.local
+# http://stackoverflow.com/a/13615531/89484
+# Now, .gitconfig can be shared across all machines and only the .local changes
 
-# symlink it up!
 ./symlink-setup.sh
 
-###
-##############################################################################################################
+# /////////////////////////////////////////////////////////////////////
