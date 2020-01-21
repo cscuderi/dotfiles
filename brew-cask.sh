9b72b04ae342192460c7854382189c512eb8a708
Carlin Scuderi <carlinscuderi@gmail.com>
2020-01-21 10:31:17 -0500

    Consolidate and cleanup some brew casks

--- brew-cask.sh
+++ brew-cask.sh
@@ -11,36 +11,58 @@ brew tap caskroom/versions
 
 # Important stufff
 brew cask install java
+brew install diff-so-fancy
 brew install git
+brew install maven
 brew install node
 brew install yarn
-brew install diff-so-fancy
 brew install zsh
-brew install maven
 
 # Dev apps
 echo "Installing dev apps..."
-brew cask install iterm2                                       # Great terminal
-brew cask install visual-studio-code                           # IDE; so good
-brew cask install docker                                       # Software containers
-brew cask install kitematic                                    # GUI for Docker
-brew cask install postman                                      # Test APIs
-brew cask install tunnelblick                                  # VPN
-
-# Productivity
-echo "Installing productivity apps..."
-brew cask install licecap                                      # High performance gif maker
-brew cask install slack                                        # Chat app
-brew cask install alfred                                       # App launcher
-brew cask install joinme                                       # Free screensharing
+brew cask install iterm2                                           # Great terminal
+brew cask install dash                                             # Dev documentation
+brew cask install postman                                          # Test APIs
+brew cask install sim-daltonism                                    # Test colourblindness
+brew cask install tower                                            # Git GUI
+brew cask install visual-studio-code                               # IDE; so good
 
 # Browsers
 echo "Installing browsers..."
-brew cask install google-chrome                                # Browser of choice
-brew cask install google-chrome-canary                         # Chrome on the edge
-brew cask install firefox                                      # Other browser
+brew cask install firefox                                          # Mozilla's browser
+brew cask install google-chrome                                    # Google's browser
+brew cask install microsoft-edge                                   # Microsoft's browser
+
+# Productivity apps
+echo "Installing productivity apps..."
+brew cask install adobe-creative-cloud                             # Adobe's app manager
+brew cask install alfred                                           # App launcher
+brew cask install bartender                                        # Menu bar icon management
+brew cask install bettertouchtool                                  # Customize keyboard, mouse, trackpad
+brew cask install cheatsheet                                       # Shows keyboard shortcuts in any app
+brew cask install colorsnapper                                     # Colour-grabbing
+brew cask install disk-inventory-x                                 # Find big junk files on your HDD
+brew cask install dropbox                                          # File syncing
+brew cask install evernote                                         # Note taking
+brew cask install google-drive-file-stream                         # File syncing
+brew cask install istat-menus                                      # Insight into hardware
+brew cask install keycastr                                         # Shows keypresses onscreen for presentations
+brew cask install licecap                                          # High performance GIF maker
+brew cask install marshallofsound-google-play-music-player         # Music streaming service
+brew cask install nordvpn                                          # VPN service
+brew cask install onepassword                                      # Password manager
+brew cask install smooze                                           # Control scrolling
+brew cask install transmit                                         # FTP client
+brew cask install vlc                                              # Plays any video/media
+
+# Social apps
+echo "Installing social apps..."
+brew cask install discord                                          # Gaming chat app
+brew cask install slack                                            # Chat app
+brew cask install tweetbot                                         # Twitter app
+brew cask install zoomus                                           # Meeting app
 
 # Cleanup
-echo "Cleaning up"
+echo "Cleaning up!"
 brew cleanup --force
 rm -f -r /Library/Caches/Homebrew/*
